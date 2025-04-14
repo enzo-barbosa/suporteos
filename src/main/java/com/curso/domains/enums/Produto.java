ecaffa3632618e2e064babbfbecbe505830e27bf
enzo-barbosa <pasquatty@gmail.com>
2025-04-13 21:25:24 -0300

    Remaking the ProductGroup class

--- src/main/java/com/curso/domains/enums/Produto.java
+++ src/main/java/com/curso/domains/enums/Produto.java
@@ -1,5 +1,6 @@
 package com.curso.domains.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
@@ -22,23 +23,47 @@ public class Produto {
     private String descricao;
 
     @NotNull
-    @Digits(integer = 15, fraction = 3)
+    @Digits(integer = 15, fraction = 3) // Define o número de inteiros e decimais (15 inteiros e 3 casas decimais)
     private BigDecimal saldoEstoque;
+
+    @NotNull
+    @Digits(integer = 15, fraction = 3) // Define o número de inteiros e decimais (15 inteiros e 3 casas decimais)
     private BigDecimal valorUnitario;
+
+    @NotNull
+    @Digits(integer = 15, fraction = 3) // Define o número de inteiros e decimais (15 inteiros e 3 casas decimais)
+    private BigDecimal valorEstoque;
+
+    @JsonFormat(pattern = "dd/MM/yyyy") // Define o formato de data que será descrita no Json para a comunicação com o front-end.
     private LocalDate dataCadastro;
+
+    @ManyToOne // Indica que muitos objetos da entidade Produto estão associados a um objeto da entidade GrupoProduto.
+    @JoinColumn(name = "idgrupoproduto") // Define a coluna na tabela produto que será usada como chave estrangeira para se referir à tabela grupoproduto
+    // O parâmetro name="idgrupoproduto" indica que a coluna no banco de dados que armazenará essa chave estrangeira se chama idgrupoproduto
+    private GrupoProduto grupoProduto;
+
+    @Enumerated(EnumType.ORDINAL) // Define que o atributo status será gravado no banco 0 ou 1 (INATIVO ou ATIVO)
+    @JoinColumn(name = "status") // Define o nome do campo no banco que irá receber o valor ordinal do Enum.
     private Status status;
 
     public Produto() {
+        this.saldoEstoque = BigDecimal.ZERO; // Estoque começa zerado
+        this.valorUnitario = BigDecimal.ZERO; // Preço unitário inicial zerado
+        this.valorEstoque = BigDecimal.ZERO; // Valor total do estoque zerado
         this.status = Status.ATIVO; // Define que quando gerar um novo produto ele será ATIVO
     }
 
-    public Produto(long idProduto, String descricao, double saldoEstoque, double valorUnitario, LocalDate dataCadastro, Status status) {
+    public Produto(long idProduto, String descricao, BigDecimal saldoEstoque, BigDecimal valorUnitario, LocalDate dataCadastro, GrupoProduto grupoProduto, Status status) {
         this.idProduto = idProduto;
         this.descricao = descricao;
-        this.saldoEstoque = saldoEstoque;
+        // this.saldoEstoque = saldoEstoque;
         this.valorUnitario = valorUnitario;
         this.dataCadastro = dataCadastro;
+        this.grupoProduto = grupoProduto;
         this.status = status;
+
+        this.saldoEstoque = saldoEstoque != null ? saldoEstoque : BigDecimal.ZERO; // Se saldoEstoque for nulo, define como ZERO
+        this.valorEstoque = saldoEstoque != null ? saldoEstoque.multiply(valorUnitario) : BigDecimal.ZERO; // Calcula o valor total do estoque (saldo * unitário) ou ZERO se saldo for nulo
     }
 
     public long getIdProduto() {
@@ -57,20 +82,28 @@ public class Produto {
         this.descricao = descricao;
     }
 
-    public double getSaldoEstoque() {
+    public BigDecimal getSaldoEstoque() {
         return saldoEstoque;
     }
 
-    public void setSaldoEstoque(double saldoEstoque) {
+    public void setSaldoEstoque(BigDecimal saldoEstoque) {
         this.saldoEstoque = saldoEstoque;
     }
 
-    public double getValorUnitario() {
+    public BigDecimal getValorUnitario() {
         return valorUnitario;
     }
 
-    public void setValorUnitario(double valorUnitario) {
+    public void setValorUnitario(BigDecimal valorUnitario) {
         this.valorUnitario = valorUnitario;
+    }
+
+    public BigDecimal getValorEstoque() {
+        return valorEstoque;
+    }
+
+    public void setValorEstoque(BigDecimal valorEstoque) {
+        this.valorEstoque = valorEstoque;
     }
 
     public LocalDate getDataCadastro() {
@@ -79,6 +112,14 @@ public class Produto {
 
     public void setDataCadastro(LocalDate dataCadastro) {
         this.dataCadastro = dataCadastro;
+    }
+
+    public GrupoProduto getGrupoProduto() {
+        return grupoProduto;
+    }
+
+    public void setGrupoProduto(GrupoProduto grupoProduto) {
+        this.grupoProduto = grupoProduto;
     }
 
     public Status getStatus() {

610eac49f57ae9de726b2566d7e3e1bf78c4395e
enzo-barbosa <pasquatty@gmail.com>
2025-04-19 15:51:07 -0300

    Creating a db files

--- src/main/java/com/curso/repositories/ProdutoRepository.java
+++ src/main/java/com/curso/repositories/ProdutoRepository.java
@@ -9,5 +9,5 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     // JpaRepository<> recebe dois parâmetros que são a classe que o repository irá gerenciar e o tipo de dado da chave da classe
     // Porque Long ao invés de long informado no id da classe GrupoProduto? O Spring Data preferem a versão wrapper por conta da flxibilidade de trabalho
 
-    
+
 }
